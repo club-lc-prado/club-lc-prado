2de551f4a7786b8b3dabf76e0daa607ea5b6c248
ScrapMarket <scrapmarket.ua@gmail.com>
2026-07-16 18:54:20 +0200

    update

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,15 @@
 import { useLanguage } from "../i18n/LanguageContext";
 import "./About.css";
+import aboutBg from "../about-bg.jpg";
 
 function About() {
   const { t } = useLanguage();
 
   return (
     <div className="about-static">
+      <div className="about-bg-fixed" style={{ backgroundImage: `url(${aboutBg})` }}></div>
+      <div className="about-bg-overlay"></div>
+
       <h1 className="about-title">{t.about.title}</h1>
       <div className="about-underline"></div>
       <div className="about-text">
