42334834f506eb5b55c95914fcad6b99b1ebade4
ScrapMarket <scrapmarket.ua@gmail.com>
2026-07-17 17:11:21 +0200

    update

--- src/pages/News.js
+++ src/pages/News.js
@@ -3,7 +3,7 @@ import newsBg from "../news-bg.jpg";
 
 function News() {
   return (
-    <div className="static-page" style={{ backgroundImage: `url(${newsBg})` }}>
+    <div className="static-page news-static-page" style={{ backgroundImage: `url(${newsBg})` }}>
       <div className="static-page-overlay"></div>
       <h1 className="static-page-title">Новости</h1>
     </div>
