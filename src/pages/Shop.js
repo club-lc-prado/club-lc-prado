0956c16ca9e978f43949ec6d9cbb5e67dba06e3d
ScrapMarket <scrapmarket.ua@gmail.com>
2026-07-17 19:51:15 +0200

    update

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,7 +1,12 @@
+import "./TechInfo.css";
+import shopBg from "../shop-bg.jpg";
+
 function Shop() {
   return (
-    <div>
-      <h1>Магазин</h1>
+    <div className="static-page">
+      <div className="static-page-bg" style={{ backgroundImage: `url(${shopBg})` }}></div>
+      <div className="static-page-overlay"></div>
+      <h1 className="static-page-title">Магазин</h1>
     </div>
   );
 }
