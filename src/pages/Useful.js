e03aaea1a6265b86bf9c2838d3751c99bc604e07
ScrapMarket <scrapmarket.ua@gmail.com>
2026-07-26 14:22:23 +0200

    update

--- src/pages/Useful.js
+++ src/pages/Useful.js
@@ -1,14 +1,43 @@
+import { useState } from "react";
 import { useLanguage } from "../i18n/LanguageContext";
-import "./TechInfo.css";
-import techInfoBg from "../techinfo-bg.jpg";
+import "./Useful.css";
+import germanyMapBg from "../germany-map-bg.jpg";
 
 function Useful() {
   const { t } = useLanguage();
+  const [clickPos, setClickPos] = useState(null);
+
+  const handleMapClick = (e) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const xPercent = ((e.clientX - rect.left) / rect.width) * 100;
+    const yPercent = ((e.clientY - rect.top) / rect.height) * 100;
+    setClickPos({ x: xPercent.toFixed(1), y: yPercent.toFixed(1) });
+  };
+
   return (
-    <div className="static-page">
-      <div className="static-page-bg" style={{ backgroundImage: `url(${techInfoBg})` }}></div>
-      <div className="static-page-overlay"></div>
-      <h1 className="static-page-title">{t.static.useful}</h1>
+    <div className="useful-page">
+      <h1 className="useful-title">{t.static.useful}</h1>
+
+      <div
+        className="useful-map-wrap"
+        style={{ backgroundImage: `url(${germanyMapBg})` }}
+        onClick={handleMapClick}
+      >
+        {clickPos && (
+          <div
+            className="useful-click-marker"
+            style={{ left: `${clickPos.x}%`, top: `${clickPos.y}%` }}
+          >
+            {clickPos.x}%, {clickPos.y}%
+          </div>
+        )}
+      </div>
+
+      {clickPos && (
+        <div className="useful-coords-hint">
+          Координаты последнего клика: x={clickPos.x}%, y={clickPos.y}%
+        </div>
+      )}
     </div>
   );
 }
